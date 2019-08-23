e69964551e72f6c3f7697bc268a73631c2920bc1
Sean Chan <sean.chanhin1@gmail.com>
2019-08-23 11:19:49 +0800

    Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -7,22 +9,43 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
+
         System.out.println("Hello from\n" + logo);
 
-        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+        Response intro = new Response("Hello! I'm Duke\n" + "What can I do for you?");
+        intro.print();
 
         //INPUT
         Scanner scan = new Scanner(System.in);
 
-        //ECHO FUNCTION
-        Response reply = new Response("");
+        //ADD LIST (LEVEL 2)
+        Response reply = new Response();
         String input;
+        int size = 1;
+        List<String> list = new ArrayList<String>();
+
         while(!(input = scan.nextLine()).equals("bye")) {
-            reply.edit(input);
-            reply.print();
+
+            reply.setContent(input);
+
+            switch(input) {
+                case "list":
+                    reply.addArray(list);
+                    reply.print();
+                    break;
+                default:
+                    list.add(Integer.toString(size) + ". " + input);
+                    reply.setContent("Added: " + input);
+                    reply.print();
+                    size++;
+                    break;
+            }
+
         }
-        reply.edit("Bye. Hope to see you again soon!");
+        reply.setContent("Bye. Hope to see you again soon!");
         reply.print();
+
 
     }
 }
--- src/main/java/Response.java
+++ src/main/java/Response.java
@@ -1,25 +1,49 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class Response {
 
     //Instance variables (Attributes)
-    private String content;
+    private List<String> content;
 
-    //Constructor
-    Response(String content) {
-        this.content = content;
+    //Constructors
+    Response() {
+        this.content = new ArrayList<String>();
+    }
+
+    Response(String str) {
+        String[] list = str.split("\\r?\\n");
+        content = Arrays.asList(list);
     }
 
     //Method 1
-    void edit(String str) {
-        content = str;
+    List<String> getContent() {
+        return content;
     }
 
     //Method 2
+    void setContent(String str) {
+        String[] list = str.split("\\r?\\n");
+        content = Arrays.asList(list);
+    }
+
+    //Method 3
+    void addArray(List<String> list) {
+        content = list;
+    }
+
+    //Method 4
     void print() {
         String wrap = "-";
-        String wrapper = "    ".concat(wrap.repeat(60));
+        String wrapper = "    " + wrap.repeat(60);
 
         System.out.println(wrapper);
-        System.out.println("    ".concat(content));
+        for (String s : content) {
+            System.out.println("    " + s);
+        }
         System.out.println(wrapper);
     }
+
+
 }
