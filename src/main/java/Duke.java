2f33b46f73a44315d41c2c19c3e02c2633547ea7
Sean Chan <sean.chanhin1@gmail.com>
2019-08-27 23:05:00 +0800

    Level-7: Save

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,103 @@
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+
 public class Duke {
+
+    private static List<Task> getTasks(String filePath) throws FileNotFoundException {
+        List<Task> tasklist = new ArrayList<Task>();
+        File f = new File(filePath);
+        Scanner s = new Scanner(f);
+
+        while(s.hasNext()) {
+            String task = s.nextLine();
+            String[] taskArray = task.split(" \\| ");
+
+            Task newTask = null;
+
+            if (taskArray[0].equals("T")) {
+                newTask = new ToDo(taskArray[taskArray.length - 1]);
+            } else if (taskArray[0].equals("D")) {
+                newTask = new Deadline(taskArray[taskArray.length - 2], taskArray[taskArray.length - 1]);
+            } else if (taskArray[0].equals("E")) {
+                newTask = new Event(taskArray[taskArray.length - 2], taskArray[taskArray.length - 1]);
+            } else {
+                try {
+                    throw new DukeException("Invalid task type");
+                } catch (DukeException e) {
+                    e.print();
+                }
+            }
+            //Check if task is done
+            if (taskArray[1].equals("1")) {
+                newTask.markAsDone();
+            }
+            tasklist.add(newTask);
+        }
+
+        return tasklist;
+    }
+
+    private static void writeTasks(String filePath, List<Task> tasklist) throws IOException {
+        FileWriter fw = new FileWriter(filePath);
+
+        for (Task t: tasklist) {
+
+            String[] lineArray = new String[4];
+
+            //Check type of task
+            if (t instanceof ToDo) {
+                lineArray[0] = "T";
+            } else if (t instanceof Deadline) {
+                lineArray[0] = "D";
+                lineArray[3] = ((Deadline) t).getBy();
+            } else if (t instanceof Event) {
+                lineArray[0] = "E";
+                lineArray[3] = ((Event) t).getAt();
+            } else {
+                try {
+                    throw new DukeException("Invalid Task: " + t.getDescription());
+                } catch (DukeException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            //Check if task is done
+            if (t.getStatusIcon() == "\u2713") {
+                lineArray[1] = "1";
+            } else if (t.getStatusIcon() == "\u2718") {
+                lineArray[1] = "0";
+            } else {
+                try {
+                    throw new DukeException("Invalid status of task: " + t.getDescription());
+                } catch (DukeException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            //Check description
+            lineArray[2] = t.getDescription();
+
+            //Combine
+            if (lineArray[3] == null) {
+                lineArray = Arrays.copyOfRange(lineArray, 0, 3);
+            }
+            String line = String.join(" | ",lineArray);
+            fw.write(line);
+            fw.write(System.lineSeparator());
+        }
+
+        fw.close();
+    }
+
     public static void main(String[] args) {
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -20,10 +113,19 @@ public class Duke {
         //INPUT
         Scanner scan = new Scanner(System.in);
 
-        //Mark as Done (LEVEL 3)
+        //Save (Level 7)
+
+        //Read file
+        List<Task> tasklist = null; //ADD FILEPATH
+        try {
+            tasklist = getTasks("/Users/seanchan/duke/data/duke.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        //List<Task> tasklist = new ArrayList<Task>();
+
         Response reply = new Response();
         String input;
-        List<Task> tasklist = new ArrayList<Task>();
 
         while(!(input = scan.nextLine()).equals("bye")) {
 
@@ -106,6 +208,12 @@ public class Duke {
                     } catch (DukeException e) {
                         e.print();
                     }
+            }
+
+            try {
+                writeTasks("/Users/seanchan/duke/data/duke.txt",tasklist);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
 
         }
